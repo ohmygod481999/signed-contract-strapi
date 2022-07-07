389a8d4c085dfe4fed375fb561900ad5b003abdf
longvb <vuongbaolong48@gmail.com>
2022-07-07 15:30:44 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,11 +1,26 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
+  // connection: {
+  //   client: 'sqlite',
+  //   connection: {
+  //     filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  //   },
+  //   useNullAsDefault: true,
+  // },
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "159.223.67.62"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "signed-contract-strapi"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "huongdangyeu"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      // ssl: {
+      //   rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+      // },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
